ade2d3448c8b98e6c60c99ed030ff941673cef1c
LUIS FERNANDO GUIMARAES SANTOS <luis.fgsantos6@senacsp.edu.br>
2025-03-25 22:14:02 -0300

    aula banco de dados terça feira

---
+++ BancoDeDados/table-endereco.sql
@@ -0,0 +1,19 @@
+CREATE TABLE if not exists endereco (
+	id int primary key auto_increment,
+    logradouro varchar(30) not null,
+    numero int(4) not null unique,
+    bairro varchar(30) not null,
+    cidade varchar(30) not null,
+    estado varchar(2) not null,
+    país  varchar(30) not null
+);
+
+SELECT 
+    *
+FROM
+    endereco;
+    
+INSERT INTO endereco (Logradouro, numero, bairro, cidade, estado, país) VALUES
+('Rua São Marcos', '195', 'Gaivotas', 'São Paulo', 'SP', 'Brasil');
+
+
